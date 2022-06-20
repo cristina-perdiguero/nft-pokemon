e146cc85946635e8d5ea16cf66916f0c2d05773f
Cristina Perdiguero Castilla <68542187+cristina-perdiguero@users.noreply.github.com>
2022-06-20 19:39:36 +0200

    maquetación card nft

--- src/app/modules/nft/components/nft-card/nft-card.component.ts
+++ src/app/modules/nft/components/nft-card/nft-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nft-card',
   templateUrl: './nft-card.component.html',
   styleUrls: ['./nft-card.component.scss']
 })
 export class NftCardComponent implements OnInit {
+
+  @Input() nft : any;
 
   constructor() { }
 
